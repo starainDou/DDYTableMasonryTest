9ee3352673e228fa8b8ac0db8eb9ba1b08c5eb20
Rain Dou <634778311@qq.com>
2018-06-12 11:48:24 +0800

    iOS11 delete image
    iOS11 delete image

--- DDYTableMasonryTest/DDYTableMasonryTest/System/ViewController.m
+++ DDYTableMasonryTest/DDYTableMasonryTest/System/ViewController.m
@@ -41,6 +41,46 @@
     return cell;
 }
 
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+}
+- (NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return @"删除";
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return (indexPath.section == 1) ? NO : YES;
+}
+
+- (NSArray *)tableView:(UITableView *)tableView editActionsForRowAtIndexPath:(NSIndexPath *)indexPath {
+    __block NSMutableArray *sectionArray = (NSMutableArray *)self.dataArray[indexPath.section];
+    __weak __typeof__ (self)weakSelf = self;
+    UITableViewRowAction *deleteAction = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleDefault
+                                                                            title:@"删除"
+                                                                          handler:^(UITableViewRowAction *action, NSIndexPath *indexPath) {
+                                                                              __strong __typeof__ (weakSelf)strongSelf = weakSelf;
+                                                                              dispatch_async(dispatch_get_global_queue(0, 0), ^{
+                                                                                  [sectionArray removeObjectAtIndex:indexPath.row];
+                                                                                  [strongSelf.tableView reloadData];
+                                                                              });
+                                                                          }];
+    return @[deleteAction];
+}
+
+/**
+ *  只有cell高度达到一定才会显示上图下文按钮否则只显示图片
+ *  UIContextualActionStyleDestructive滑到对边删除
+ */
+- (UISwipeActionsConfiguration *)tableView:(UITableView *)tableView trailingSwipeActionsConfigurationForRowAtIndexPath:(NSIndexPath *)indexPath  API_AVAILABLE(ios(11.0)) {
+    UIContextualAction *deleteRowAction = [UIContextualAction contextualActionWithStyle:UIContextualActionStyleDestructive title:@"delete" handler:^(UIContextualAction * _Nonnull action, __kindof UIView * _Nonnull sourceView, void (^ _Nonnull completionHandler)(BOOL)) {
+        completionHandler (YES);
+    }];
+    deleteRowAction.title = @"删除";
+    deleteRowAction.image = [UIImage imageNamed:@"Delete"];
+    deleteRowAction.backgroundColor = [UIColor redColor];
+    UISwipeActionsConfiguration *config = [UISwipeActionsConfiguration configurationWithActions:@[deleteRowAction]];
+    return config;
+}
+
 - (void)loadTestData {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         for (int i = 0; i < 5; i++) {
